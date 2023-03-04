603944ee08db3c9bf5e130cf53accc6c4d32dc17
Nikola Vujovic <NikolaVujovic@Nikolas-MBP.fritz.box>
2023-03-04 23:58:36 +0100

    added JSON

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Koji je glavni grad Srbije",
+        "answer_1": "Kraljevo",
+        "answer_2": "S.Palanka",
+        "answer_3": "Mladenovac",
+        "answer_4": "Beograd",
+        "right_answer": 4
+    },
+    {
+        "question": "Koja reka protice kroz Srbiju",
+        "answer_1": "Dunav",
+        "answer_2": "Rajna",
+        "answer_3": "Volga",
+        "answer_4": "Moraca",
+        "right_answer": 1
+    },
+    {
+        "question": "Koji Srbin je izumeo struju",
+        "answer_1": "J.J. Zmaj",
+        "answer_2": "Nikola Tesla",
+        "answer_3": "Milutin Milankovic",
+        "answer_4": "Ivo Andric",
+        "right_answer": 2
+    },
+    {
+        "question": "Ko je najuspesniji srpski Teniser",
+        "answer_1": "Novak Djokovic",
+        "answer_2": "Janko Tipsarevic",
+        "answer_3": "Viktor Troicki",
+        "answer_4": "Nenad Zimonjic",
+        "right_answer": 1
+    },
+    {
+        "question": "Koja Srpska rec se koristi svuda u svetu",
+        "answer_1": "Kafa",
+        "answer_2": "Vampir",
+        "answer_3": "Rakija",
+        "answer_4": "Sunce",
+        "right_answer": 2
+    }
+];
