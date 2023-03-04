e212947a1b98073e2b3c3a275d7d8bf759fd37c4
Nikola Vujovic <NikolaVujovic@Nikolas-MBP.fritz.box>
2023-03-05 00:59:23 +0100

    added Question and Answers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
     {
-        "question": "Koji je glavni grad Srbije",
+        "question": "Koji je glavni grad Srbije?",
         "answer_1": "Kraljevo",
         "answer_2": "S.Palanka",
         "answer_3": "Mladenovac",
@@ -8,7 +8,7 @@ let questions = [
         "right_answer": 4
     },
     {
-        "question": "Koja reka protice kroz Srbiju",
+        "question": "Koja reka protice kroz Srbiju?",
         "answer_1": "Dunav",
         "answer_2": "Rajna",
         "answer_3": "Volga",
@@ -16,7 +16,7 @@ let questions = [
         "right_answer": 1
     },
     {
-        "question": "Koji Srbin je izumeo struju",
+        "question": "Koji Srbin je izumeo struju?",
         "answer_1": "J.J. Zmaj",
         "answer_2": "Nikola Tesla",
         "answer_3": "Milutin Milankovic",
@@ -24,7 +24,7 @@ let questions = [
         "right_answer": 2
     },
     {
-        "question": "Ko je najuspesniji srpski Teniser",
+        "question": "Ko je najuspesniji srpski Teniser?",
         "answer_1": "Novak Djokovic",
         "answer_2": "Janko Tipsarevic",
         "answer_3": "Viktor Troicki",
@@ -32,11 +32,33 @@ let questions = [
         "right_answer": 1
     },
     {
-        "question": "Koja Srpska rec se koristi svuda u svetu",
+        "question": "Koja Srpska rec se koristi svuda u svetu?",
         "answer_1": "Kafa",
         "answer_2": "Vampir",
         "answer_3": "Rakija",
         "answer_4": "Sunce",
         "right_answer": 2
     }
-]
+];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('questionsLength').innerHTML = questions.length;
+    showQuestion();
+
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    
+}
